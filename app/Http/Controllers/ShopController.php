9fbc2ceeb123146bd218dbfe5864955df56e0f4c
Sitt Aung <sittaung07@gmail.com>
2018-07-20 02:20:05 +0630

    Categories and pagination

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -14,9 +15,29 @@ class ShopController extends Controller
      */
     public function index()
     {
-        $products = Product::inRandomOrder()->take(12)->get();
+        $pagination = 9;
+        $categories = Category::all();
 
-        return view('shop', compact('products'));
+        if (request()->category) {
+            $products = Product::with('categories')->whereHas('categories', function ($query) {
+                $query->where('slug', request()->category);
+            });
+
+            $categoryName = optional($categories->where('slug', request()->category)->first())->name;
+        } else {
+            $products = Product::where('featured', true);
+            $categoryName = 'Featured';
+        }
+
+        if (request()->sort == 'low_high') {
+            $products = $products->orderBy('price')->paginate($pagination);
+        } elseif (request()->sort == 'high_low') {
+            $products = $products->orderBy('price', 'desc')->paginate($pagination);
+        } else {
+            $products = $products->paginate($pagination);
+        }
+
+        return view('shop', compact('products', 'categories', 'categoryName'));
     }
 
     /**
